f73c20e2320cbab3b9ccc5c1862d38058d489fa5
Milan Ravenell <Milan Ravenell>
2017-01-10 12:50:41 +0800

    pull

--- laotu/laotu.py
+++ laotu/laotu.py
@@ -16,7 +16,7 @@ import os
 from flask_sqlite_admin.core import sqliteAdminBlueprint
 
 # configuration
-DATABASE = '/tmp/laotu.db'
+DATABASE = 'C:\\Users\\Milan\\Documents\\Harvard\\fall 2016\\d4d\\LaotuRepo\\laotu\\tmp\\laotu.db'
 # DATABASE = 'C:\\Users\\samzliu\\Desktop\\LaoTu\\LaoTu\\laotu\\tmp\\laotu.db'
 PER_PAGE = 30
 DEBUG = True
