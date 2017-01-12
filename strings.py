d78f8554b96bdc5068520e75b146c66bb67b9ca4
Carl Denton <carldenton@college.harvard.edu>
2017-01-12 10:48:12 +0800

    added strings.py back

---
+++ strings.py
@@ -0,0 +1,28 @@
+"""
+	strings for laotu.py
+
+"""
+
+#Error messages ...............................................................
+ERR_INVALID_EMAIL = 'Invalid email'
+ERR_INVALID_PWD   = 'Invalid password'
+ERR_NO_NAME    = 'You have to enter a name'
+ERR_NO_ADDRESS = 'You have to enter an address'
+ERR_NO_PHONE   = 'You have to enter a phone number'
+ERR_INVALID_PHONE = 'Invalid phone number'
+ERR_NO_EMAIL = 'You have to enter an email address'
+ERR_NO_PWD   = 'You have to enter a password and confirm it'
+ERR_MISMATCH = 'The two passwords do not match'
+ERR_EMAIL_TAKEN = 'The email is already taken'
+
+
+#Flashes ......................................................................
+FLASH_LOGGED = 'You were logged in'
+FLASH_REGISTERED = 'You were succesfully registered and can log in now'
+FLASH_UNLOGGED = 'You were logged out'
+FLASH_SIGNIN_NEEDED = 'You must sign in first to access this functionality'
+FLASH_CARTED = 'The product has been added to the cart'
+FLASH_UNCARTED = 'The product has been removed from the cart'
+FLASH_CLEARED = 'The cart has been cleared'
+FLASH_UPDATED = 'The cart has been updated'
+FLASH_PURCHASE = 'Your purchase was succesful'
