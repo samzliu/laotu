d22944daa9b5d05135d4fc326f92b707934e4dc7
Milan Ravenell <Milan Ravenell>
2017-01-13 13:59:09 +0800

    UI changes

--- laotu/laotu.py
+++ laotu/laotu.py
@@ -20,7 +20,7 @@ from flask.ext.uploads import UploadSet, IMAGES, configure_uploads
 from strings import *
 
 # configuration
-DATABASE = 'C:\\Users\\Milan\\Documents\\Harvard\\fall2016\\d4d\\LaotuRepo\\laotu\\tmp\\laotu.db'
+DATABASE = 'C:\\Users\\Milan\\Documents\\Harvard\\fall 2016\\d4d\\LaotuRepo\\laotu\\tmp\\laotu.db'
 #DATABASE = '/tmp/laotu.db'
 #DATABASE = 'C:\\Users\\samzliu\\Desktop\\LaoTu\\LaoTu\\laotu\\tmp\\laotu.db'
 PER_PAGE = 30
